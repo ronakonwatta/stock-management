521cab8e6ad97193347ce8a74b5f4a912b5a7307
Ronakon Watt <88375612+ronakon45@users.noreply.github.com>
2023-10-11 22:22:18 +0700

    [UPDATE] ProductService

--- src/main/java/online/ronakon/stockmanagement/ProductRepository.java
+++ src/main/java/online/ronakon/stockmanagement/ProductRepository.java
@@ -1,9 +1,13 @@
 package online.ronakon.stockmanagement;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 @Repository
-public interface ProductRepository extends CrudRepository<Product, String> {
-    Product findByNameEquals(String productName);
+public interface ProductRepository extends ListCrudRepository<Product, Long> {
+    Product findByProductCode(String productCode);
 }
---
+++ src/main/java/online/ronakon/stockmanagement/ProductService.java
@@ -0,0 +1,48 @@
+package online.ronakon.stockmanagement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository repository;
+
+    public ProductService() {
+    }
+
+
+    void save(Product product) {
+        Product _oldProduct = repository.findByProductCode(product.productCode);
+        if (_oldProduct == null)
+            repository.save(product);
+        if (_oldProduct != null) {
+            _oldProduct.productName = product.productName;
+            _oldProduct.lastModifiedDate = new Date();
+            _oldProduct.price = product.price;
+            repository.save(_oldProduct);
+        }
+    }
+//
+//    void delete(Product product) {
+//        Product _oldProduct = repository.findByProductCode(product.productCode);
+//        if (_oldProduct == null)
+//            return;
+//        if (_oldProduct != null) {
+//            _oldProduct.deletedDate = new Date();
+//            _oldProduct.isDeleted = Boolean.TRUE;
+//            repository.save(_oldProduct);
+//        }
+//    }
+
+    List<Product> findAll() {
+        return repository.findAll();
+    }
+
+}
