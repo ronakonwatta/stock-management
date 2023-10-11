b8b1f808614952a2d9389dda8ec3b254f073bb05
Ronakon Watt <88375612+ronakon45@users.noreply.github.com>
2023-10-12 01:35:06 +0700

    [UPDATE] STOCK

--- src/main/java/online/ronakon/stockmanagement/StockRepository.java
+++ src/main/java/online/ronakon/stockmanagement/StockRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.TreeSet;
 
 @Repository
 public interface StockRepository extends ListCrudRepository<Stock, Long> {
-    List<Stock> findByCompanyOrderByCreateTimeDesc(String companyCode);
+    List<Stock> findByCompanyOrderByCreateTimeDesc (String companyCode);
 }
