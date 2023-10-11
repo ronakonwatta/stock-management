ee882173e9feecb33d0f12d0473a98c206ba631d
Ronakon Watt <88375612+ronakon45@users.noreply.github.com>
2023-10-11 09:10:43 +0700

    Entity Product

--- src/main/java/online/ronakon/stockmanagement/Product.java
+++ src/main/java/online/ronakon/stockmanagement/Product.java
@@ -1,3 +1,72 @@
 package online.ronakon.stockmanagement;
 
-record Product(String name, String size, double price) {}
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    private double price;
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Product() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product product)) return false;
+        return Double.compare(getPrice(), product.getPrice()) == 0 && Objects.equals(getId(), product.getId()) && Objects.equals(getName(), product.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getPrice());
+    }
+}
