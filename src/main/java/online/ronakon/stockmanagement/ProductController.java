21d5edbecebb44ca89f9d9506486ebfca213ad1b
Ronakon Watt <88375612+ronakon45@users.noreply.github.com>
2023-10-11 16:36:53 +0700

    [ADD] entityWithLocation

--- src/main/java/online/ronakon/stockmanagement/ProductController.java
+++ src/main/java/online/ronakon/stockmanagement/ProductController.java
@@ -1,9 +1,13 @@
 package online.ronakon.stockmanagement;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,15 +18,20 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;
 
-    @GetMapping( "/all")
-    List<Product> getAllProduct(){
-        return (List<Product>) productRepository.findAll();
+    @GetMapping("/all")
+    ResponseEntity<List<Product>> getAllProduct() {
+        return ResponseEntity.ok((List<Product>) productRepository.findAll());
     }
 
     @PostMapping("/add")
-    void addProduct(@RequestBody Product product){
-        productRepository.save(product);
-        return;
+    ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        return ResponseEntity.status(201).body(productRepository.save(product));
+    }
+
+    @PostMapping("/add-all")
+    ResponseEntity<Iterable<Product>> addProduct(@RequestBody List<Product> productList) {
+        Iterable<Product> iterableProductList = productList;
+        return ResponseEntity.status(201).body(productRepository.saveAll(iterableProductList));
     }
 
     @PutMapping("/{id}")
@@ -31,13 +40,32 @@ public class ProductController {
     }
 
     @PatchMapping("/{id}")
-    void patchProduct(@PathVariable("id") String id, @RequestBody Product product){
+    void patchProduct(@PathVariable("id") String id, @RequestBody Product product) {
         productRepository.save(product);
     }
 
     @DeleteMapping("/{id}")
-    void delProduct(@PathVariable("id") String id) {
-        productRepository.deleteById(id);
+    ResponseEntity deleteProduct(@PathVariable("id") String id) {
+        return  ResponseEntity.status(HttpStatus.NO_CONTENT).body("id remove" + id);
     }
 
+
+    /**
+     * Return a response with the location of the new resource.
+     *
+     * Suppose we have just received an incoming URL of, say,
+     * http://localhost:8080/accounts and resourceId is "1111". Then the URL of the
+     * new resource will be http://localhost:8080/accounts/1111.
+     */
+    private ResponseEntity<Void> entityWithLocation(Object resourceId) {
+
+        // TODO-07: Set the 'location' header on a Response to URI of
+        // the newly created resource and return it.
+        // a. You will need to use 'ServletUriComponentsBuilder' and
+        // 'ResponseEntity' to implement this - Use ResponseEntity.created(..)
+        // b. Refer to the POST example in the slides for more information
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(resourceId).toUri();
+        return ResponseEntity.created(uri).build(); // Return something other than null
+    }
 }
