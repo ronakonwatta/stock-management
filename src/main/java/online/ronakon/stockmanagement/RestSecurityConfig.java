861bf6bf2870e1dc0f1380b2dad7a6357ed32f53
Ronakon Watt <88375612+ronakon45@users.noreply.github.com>
2023-10-18 12:04:27 +0700

    Deprecated old class

--- src/main/java/online/ronakon/stockmanagement/RestSecurityConfig.java
+++ src/main/java/online/ronakon/stockmanagement/RestSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Deprecated
 @Configuration
 @EnableMethodSecurity
 public class RestSecurityConfig {
