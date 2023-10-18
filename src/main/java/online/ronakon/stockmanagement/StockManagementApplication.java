74ff260e969453110d642b4e56a2414e025ee446
Ronakon Watt <88375612+ronakon45@users.noreply.github.com>
2023-10-18 12:01:40 +0700

    remove bug for 401

--- src/main/java/online/ronakon/stockmanagement/StockManagementApplication.java
+++ src/main/java/online/ronakon/stockmanagement/StockManagementApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(RestSecurityConfig.class)
 public class StockManagementApplication {
 
 	public static void main(String[] args) {
